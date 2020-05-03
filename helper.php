05ed34e22e69bae126ef7ed88809e247d23adde4
ivan006 <ivan.copeland2015@gmail.com>
2020-05-03 17:16:34 +0200

    1

--- helper.php
+++ helper.php
@@ -8,18 +8,21 @@ function install(){
   // );
   $commands = array(
     "pwd",
-    "ls",
-    "cd ../",
+    "ls -la ..",
+    // "cd ../",
+    "mv ../Reportify_Installer ../../",
+    // "mv ../Reportify_Installer ../",
+    // "cp ../Reportify_Installer ./temp",
     "pwd",
-    "ls",
+    "ls -la",
   );
-  $responce = run_command($commands);
+  $responce = run_commands($commands);
   echo "<pre>";
   echo json_encode($responce,JSON_PRETTY_PRINT);
   echo "</pre>";
 }
 
-function run_command($commands){
+function run_commands($commands){
   $responce = array();
   foreach ($commands as $key => $command) {
     $responce[$key] = shell_exec($command);
